efebd09cc801e26f23fada386d0b126c29d9934d
BattleNinja <mucherzhang@gmail.com>
2017-04-17 14:49:23 -0400

    almost done

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,53 @@
 import React, { Component } from 'react';
+const marked = require('marked');
 
 export default class App extends Component {
-  render() {
-    return (
-      <div>React simple starter</div>
-    );
-  }
+    constructor(props){
+        super(props);
+        this.state={
+            value:' # A large heading \r ## Next largest \r ###### Smallest \
+                  \r\r **This is bold text**  \r __So is this__  \
+                  \r\r *This text is italicized*  \r _So is this_  \
+                  \r\r ~~This was mistaken text~~  \
+                  \r\r To quote use  \r > to denote the quoted text  \
+                  \r\r Use `backticks` for codes or commands  \
+                  \r Use \r ``` \r triple backticks \r for multiple lines \r of code \r ```  \
+                  \r\r Create inline links like so  \
+                  \r This app was built using [React](https://facebook.github.io/react/index.html)  \
+                  \r\r Unordered lists\
+                  \r\r * Fish  \r * Chips  \r * Peas\
+                  \r\r Nested & ordered Lists\
+                  \r\r1. Breakfast\r    - Toast\r    - Cereal  \r2. Dinner\r    * Fish & Chips\r      - Peas\r    * Pie & Chips'
+        }
+
+    }
+    handlechange(event){
+        this.setState({
+            value: event.target.value
+        });
+    }
+    createMarkup() {
+        return {__html: marked(this.state.value)};
+    }
+    markeddownvalue() {
+        return <div dangerouslySetInnerHTML={this.createMarkup()} />;
+    }
+
+
+    render() {
+        return (
+            <div className="text-center">
+                <h1>Markdown previewer</h1>
+                <div className ="row">
+                    <div className="col-xs-6">
+                      <textarea value={this.state.value} onChange={(event)=>{this.handlechange(event)}} />
+                    </div>
+                    <div className="col-xs-6">
+                        <h1>Output</h1>
+                        {this.markeddownvalue()}
+                    </div>
+              </div>
+          </div>
+        );
+    }
 }
